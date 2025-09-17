19b78746d65af4cfadf4fc4c8b0c0c0ebe91dfa5
rishikeshvajre <rishikeshvajre7@gmail.com>
2025-09-17 12:45:36 +0530

    some updates in config and adding .env for credentials storage

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,28 @@
 const { defineConfig } = require("cypress");
 import { allureCypress } from "allure-cypress/reporter";
 
+//const env = process.env.CYPRESS_ENV;
+
 module.exports = defineConfig({
+  // retries: 2,
+  // video: true,
+  // screenshotOnRunFailure: true,
+  // reporter: "mochawesome",
+  // reporterOptions: {
+  //   reportDir: "cypress/reports",
+  //   overwrite: false,
+  //   html: true,
+  //   json: true,
+  // },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
-      allureCypress (on, config, {
+      allureCypress(on, config, {
         resultsDir: "allure-results",
-      })
+      });
     },
   },
+  //env: require(`./config/${env}.env.js`),
 });
+
+// Central config for baseUrl, retries, reporter, etc.
